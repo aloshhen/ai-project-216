02cfcf59618263d86f4a954d3db6c24550e225f1
aloshhen <166549924+aloshhen@users.noreply.github.com>
2026-02-03 17:09:03 +0100

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        dota: {
+          red: '#E74C3C',
+          dark: '#0D0D12',
+          gray: '#1A1A24',
+          accent: '#FF6B35',
+          blue: '#3498DB',
+          green: '#2ECC71',
+          purple: '#9B59B6',
+          gold: '#F1C40F'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
